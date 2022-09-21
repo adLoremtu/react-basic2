5af99cc7d52857ffbcea4575fd9e89f2dc8211ab
takuya-inoue <takuya.inoue@waku-2.com>
2022-09-21 21:01:33 +0900

    問題2のanswerを一部修正

--- react-basic-answer/src/components/answer/No2/index.tsx
+++ react-basic-answer/src/components/answer/No2/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import SampleList from "../../ui/list";
 
 const No2 = () => {
+  const name = "ボン";
+  const age = 29;
+  const isMen = true;
+
   return (
     <>
       <h2>問題2:</h2>
       {/* === 問題2の解答エリア  start  === */}
-      <SampleList name={"ぼん"} age={29} isMen={true}/>
+      <SampleList name={name} age={age} isMen={isMen} />
       {/* === 問題2の解答エリア  end  === */}
     </>
   );
